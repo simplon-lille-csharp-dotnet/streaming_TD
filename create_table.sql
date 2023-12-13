fe5c79680bf9726cc5c22752f1deebca540c6137
Théo Duflos <theo.duflos.yokoko@gmail.com>
2023-12-13 11:49:33 +0100

    feat :sparkles: Create table Movie

--- create_table.sql
+++ create_table.sql
@@ -3,3 +3,10 @@ CREATE TABLE "user"(
 	email varchar(100),
 	"password" varchar(30)
 );
+
+CREATE TABLE movie(
+	movie_id int primary key,
+	title varchar(100),
+	duration INTERVAL,
+	release_year int
+);
