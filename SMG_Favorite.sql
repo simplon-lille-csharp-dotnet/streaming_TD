f25ac931f9b8b848d0a362486316f52c05433fa1
Théo Duflos <theo.duflos.yokoko@gmail.com>
2023-12-14 09:13:37 +0100

    feat :sparkles: Add data into tables and modify some fields

---
+++ SMG_Favorite.sql
@@ -0,0 +1,20 @@
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (1,807);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (1,986);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (1,1121);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (1,844);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (1,1547);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (1,950);
+
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (2,807);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (2,1276);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (2,1351);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (2,1254);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (2,1567);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (2,1780);
+
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (3,807);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (3,985);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (3,925);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (3,945);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (3,948);
+insert into smg_favorite(SFA_USERID,SFA_MOVIEID) VALUES (3,855);
